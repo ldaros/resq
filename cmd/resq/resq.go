6d54a2fbbe2ae206c04290d1de63f5aa03b2c4d8
Luan Daros <darosluan@gmail.com>
2024-06-07 18:20:26 -0300

    add flags for debugging

--- cmd/resq/resq.go
+++ cmd/resq/resq.go
@@ -17,57 +17,125 @@ import (
 func main() {
 	args := arguments.ParseArguments()
 
-	data, err := os.ReadFile(args.SchemaFile)
-	utils.CheckError(err, "reading request schema file")
+	data := readFile(args.SchemaFile)
+	schema := parseRequestSchema(data)
+	req := createRequest(schema)
 
-	schema, err := request.ParseRequestSchema(data)
-	utils.CheckError(err, "parsing request schema")
-
-	req, err := request.CreateRequest(schema)
-	utils.CheckError(err, "creating request")
+	printRequest(req, args)
 
 	start := time.Now()
-	resp, err := request.ExecuteRequest(req)
-	utils.CheckError(err, "executing request")
+	resp := executeRequest(req)
 	elapsed := time.Since(start)
 
 	printResponse(args, resp, elapsed)
 }
 
+func readFile(filePath string) []byte {
+	data, err := os.ReadFile(filePath)
+	utils.CheckError(err, "reading request schema file")
+	return data
+}
+
+func parseRequestSchema(data []byte) request.RequestSchema {
+	schema, err := request.ParseRequestSchema(data)
+	utils.CheckError(err, "parsing request schema")
+	return schema
+}
+
+func createRequest(schema request.RequestSchema) *http.Request {
+	req, err := request.CreateRequest(schema)
+	utils.CheckError(err, "creating request")
+	return req
+}
+
+func executeRequest(req *http.Request) *http.Response {
+	resp, err := request.ExecuteRequest(req)
+	utils.CheckError(err, "executing request")
+	return resp
+}
+
+func printRequest(req *http.Request, args arguments.CommandLineArguments) {
+	if args.ShowRequestURL {
+		fmt.Printf("Request URL:\n%s\n\n", req.URL.String())
+	}
+
+	if args.ShowRequestBody {
+		fmt.Println("Request body:")
+		body, err := io.ReadAll(req.Body)
+		utils.CheckError(err, "reading request body")
+		fmt.Printf("%s\n\n", string(body))
+		req.Body = io.NopCloser(strings.NewReader(string(body)))
+	}
+
+	if args.ShowRequestHeaders {
+		fmt.Println("Request headers:")
+		for name, values := range req.Header {
+			for _, value := range values {
+				fmt.Printf("%s: %s\n", name, value)
+			}
+		}
+		fmt.Println()
+	}
+}
+
 func printResponse(args arguments.CommandLineArguments, resp *http.Response, elapsed time.Duration) {
 	defer resp.Body.Close()
 
+	if args.ShowResponseHeaders {
+		fmt.Println("Response headers:")
+		for name, values := range resp.Header {
+			for _, value := range values {
+				fmt.Printf("%s: %s\n", name, value)
+			}
+		}
+		fmt.Println()
+	}
+
+	body := readResponseBody(resp)
+
+	statusStr, elapsedStr := formatResponseDetails(resp.StatusCode, elapsed)
+
+	output := formatOutput(resp, body)
+
+	if args.OutputFile != "" {
+		writeToFile(args.OutputFile, output)
+	} else {
+		fmt.Println(string(utils.LimitLines(output, args.NumberOfLines)))
+		fmt.Printf("Request finished with status code %s in %s\n",
+			color.Colorize(statusStr, color.GetHTTPStatusColors(http_utils.Status(resp.StatusCode).Code())),
+			color.Colorize(elapsedStr, color.GetExecutionTimeColor(elapsed.Milliseconds())))
+	}
+}
+
+func readResponseBody(resp *http.Response) []byte {
 	body, err := io.ReadAll(resp.Body)
 	utils.CheckError(err, "reading response body")
+	return body
+}
 
-	statusCode := http_utils.Status(resp.StatusCode)
-	statusColor := color.GetHTTPStatusColors(statusCode.Code())
-	statusStr := fmt.Sprintf("%d (%s)", resp.StatusCode, statusCode.String())
-
-	elapsedColor := color.GetExecutionTimeColor(elapsed.Milliseconds())
+func formatResponseDetails(statusCode int, elapsed time.Duration) (string, string) {
+	status := http_utils.Status(statusCode)
+	statusStr := fmt.Sprintf("%d (%s)", statusCode, status.String())
 	elapsedStr := elapsed.String()
+	return statusStr, elapsedStr
+}
 
+func formatOutput(resp *http.Response, body []byte) []byte {
 	var output []byte
-
 	if strings.Contains(resp.Header.Get("Content-Type"), "application/json") {
-		output, err = utils.PrettyJSON(body)
-
+		formatted, err := utils.PrettyJSON(body)
 		if err != nil {
 			output = body
+		} else {
+			output = formatted
 		}
 	} else {
 		output = body
 	}
+	return output
+}
 
-	if args.OutputFile != "" {
-		err := os.WriteFile(args.OutputFile, output, 0644)
-		utils.CheckError(err, "writing response to file")
-		return
-	}
-
-	fmt.Println(string(utils.LimitLines(output, args.NumberOfLines)))
-
-	fmt.Printf("Request finished with status code %s in %s\n",
-		color.Colorize(statusStr, statusColor),
-		color.Colorize(elapsedStr, elapsedColor))
+func writeToFile(outputFile string, output []byte) {
+	err := os.WriteFile(outputFile, output, 0644)
+	utils.CheckError(err, "writing response to file")
 }
--- pkg/arguments/args.go
+++ pkg/arguments/args.go
@@ -7,28 +7,83 @@ import (
 )
 
 type CommandLineArguments struct {
-	SchemaFile    string
-	OutputFile    string
-	NumberOfLines int
+	SchemaFile          string
+	OutputFile          string
+	NumberOfLines       int
+	ShowResponseHeaders bool
+	ShowRequestHeaders  bool
+	ShowRequestBody     bool
+	ShowRequestURL      bool
 }
 
 func ParseArguments() CommandLineArguments {
 	var (
-		schemaFile    string
-		outputFile    string
-		numberOfLines int = 30
+		schemaFile         string
+		outputFile         string
+		numberOfLines      int = 30
+		showRequestString  string
+		showResponseString string
 	)
 
-	flag.StringVar(&schemaFile, "f", "", "Path to request definition file")
-	flag.StringVar(&outputFile, "o", "", "Output file for the response")
-	flag.IntVar(&numberOfLines, "l", 30, "Number of lines to print for JSON response")
+	flag.StringVar(&schemaFile, "r", "", "Path to the request definition YAML file")
+	flag.StringVar(&outputFile, "o", "", "Path to the output file where the response will be saved")
+	flag.IntVar(&numberOfLines, "l", 30, "Number of lines to display from the JSON response")
+	flag.StringVar(&showRequestString, "rq", "", "Specify which parts of the request to show: headers (h), body (b), URL (u)")
+	flag.StringVar(&showResponseString, "rs", "", "Specify which parts of the response to show: headers (h)")
 
 	flag.Parse()
 
 	if schemaFile == "" {
-		fmt.Println("Please provide the request schema file using the -f flag")
+		fmt.Println("Provide a path to the request definition YAML file using the -r flag")
 		os.Exit(1)
 	}
 
-	return CommandLineArguments{SchemaFile: schemaFile, OutputFile: outputFile, NumberOfLines: numberOfLines}
+	showRequestHeaders, showRequestBody, showRequestURL := parseRequestFlags(showRequestString)
+	showResponseHeaders := parseResponseFlags(showResponseString)
+
+	return CommandLineArguments{
+		SchemaFile:          schemaFile,
+		OutputFile:          outputFile,
+		NumberOfLines:       numberOfLines,
+		ShowResponseHeaders: showResponseHeaders,
+		ShowRequestHeaders:  showRequestHeaders,
+		ShowRequestBody:     showRequestBody,
+		ShowRequestURL:      showRequestURL,
+	}
+}
+
+func parseRequestFlags(flagString string) (bool, bool, bool) {
+	var headers, body, url bool
+
+	for _, c := range flagString {
+		switch c {
+		case 'h':
+			headers = true
+		case 'b':
+			body = true
+		case 'u':
+			url = true
+		default:
+			fmt.Println("Invalid argument for -rq")
+			os.Exit(1)
+		}
+	}
+
+	return headers, body, url
+}
+
+func parseResponseFlags(flagString string) bool {
+	var headers bool
+
+	for _, c := range flagString {
+		switch c {
+		case 'h':
+			headers = true
+		default:
+			fmt.Println("Invalid argument for -rs")
+			os.Exit(1)
+		}
+	}
+
+	return headers
 }
